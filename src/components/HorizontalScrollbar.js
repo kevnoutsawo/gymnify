c96d6668b0f2ac06a983e24fdc4624dc3a9bd9dd
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2022-11-20 21:42:06 +0100

    Implemented a flag to handle more data types in the component

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -3,6 +3,7 @@ import React, { useContext } from 'react'
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
 
 import BodyPart from './BodyPart'
+import ExerciseCard from './ExerciseCard'
 import RightArrowIcon from '../assets/icons/right-arrow.png'
 import LeftArrowIcon from '../assets/icons/left-arrow.png'
 
@@ -26,7 +27,7 @@ const RightArrow = () => {
   )
 }
 
-const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
         {data.map((item) => (
@@ -36,7 +37,7 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
                 title={item.id || item}
                 m="0 40px"
             >
-                <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+              {isBodyParts ? <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} /> : <ExerciseCard exercise={item} />}
             </Box>
         ))}
     </ScrollMenu>
